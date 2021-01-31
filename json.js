5659d44c26ea643fbb65c1a6e4b5bbacb40a0fea
뭉이 <clapmean@gmail.com>
2021-01-31 23:56:34 +0900

    10

---
+++ json.js
@@ -0,0 +1,42 @@
+//1. Object to JSON
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+    name : 'tori',
+    color : 'white',
+    size : null,
+    birthDate : new Date(),
+    // symbol : Symbol("id"),
+    jump : ()=>{
+        console.log(`${name} can jump!`);
+    },
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+json = JSON.stringify(rabbit, ['name', 'color', 'size']);
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key : ${key}, value : ${value}`);
+    return key === 'name' ? 'moong2' : value;
+});
+console.log(json);
+
+//2. JSON to Object
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json, (key, value)=>{
+    console.log(`key : ${key}, value : ${value}`);
+    return key === 'birthDate' ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
+// obj.jump();
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
